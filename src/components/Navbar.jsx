ecc58b2822216ec48961706ee10e688ff9d8593e
Justin <justinjones38@myyahoo.com>
2026-07-16 08:42:00 -0500

    added close button to navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,56 @@
-import styles from "./Navbar.module.css"
-import { NavLink } from "react-router"
+import styles from "./Navbar.module.css";
+import { NavLink } from "react-router";
 import { useState } from "react";
 import { GiHamburgerMenu } from "react-icons/gi";
+import { IoMdClose } from "react-icons/io";
 
 
 export default function Navbar() {
   const [isMenuShown, setIsMenuShown] = useState(false);
   return (
     <div className={styles.container}>
-      <div className={styles.hamburgerMenu}>
         <GiHamburgerMenu className={styles.hamburgerBtn} />
-      </div>
+        <div className={styles.navContainer}>
+          <div className={styles.closeBtn}><IoMdClose /></div>
+          <ul className={styles.navbar}>
+            <li className={styles.navList}>
+              <NavLink
+                to="."
+                className={({ isActive }) =>
+                  isActive
+                    ? `${styles["navLink"]} ${styles["active"]}`
+                    : styles["navLink"]
+                }
+              >
+                Home
+              </NavLink>
+            </li>
+            <li className={styles.navList}>
+              <NavLink
+                to="#"
+                className={({ isActive }) =>
+                  isActive
+                    ? `${styles["navLink"]} ${styles["active"]}`
+                    : styles["navLink"]
+                }
+              >
+                Create a Crewmare
+              </NavLink>
+            </li>
+            <li className={styles.navList}>
+              <NavLink
+                to="#"
+                className={({ isActive }) =>
+                  isActive
+                    ? `${styles["navLink"]} ${styles["active"]}`
+                    : styles["navLink"]
+                }
+              >
+                Crewmate Gallery
+              </NavLink>
+            </li>
+          </ul>
+        </div>
     </div>
-  )
+  );
 }
