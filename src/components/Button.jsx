3bd3b20550fac6bea5ecae30e0509b8cfd4eca87
Justin <justinjones38@myyahoo.com>
2026-07-16 20:02:53 -0500

    added fetched data from Gallery

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,8 +1,8 @@
 import styles from "./Button.module.css"
 
-export default function Button({children}) {
+export default function Button({children}, ...restProps) {
   return (
-    <button className={styles.btn}>{children}</button>
+    <button className={styles.btn} {...restProps}>{children}</button>
   )
 
 }
--- src/pages/Gallery.jsx
+++ src/pages/Gallery.jsx
@@ -1,9 +1,52 @@
-import styles from "./Gallery.module.css"
+import { useEffect, useState } from "react";
+import styles from "./Gallery.module.css";
+import { supabase } from "../services/client";
+import Button from "../components/Button";
 
 export default function Gallery() {
+  const [crewmates, setCrewmates] = useState(null);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const { data, error } = await supabase.from("crewmates").select();
+
+      if (error) {
+        setError(true);
+        return;
+      }
+      setCrewmates(data);
+    };
+    fetchData();
+  }, []);
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>Your Crewmate Gallery!</h1>
+      {error ? <h2>Cannot fetch data</h2> : null}
+      {!error && crewmates ? (
+        <div className={styles.cardContainer}>
+          {crewmates.map((item) => (
+            <div
+              className={styles.card}
+              style={{ border: `1px solid ${item["color"]}` }}
+              key={item.id}
+            >
+              <div className={styles.itemDescriptionContainer}>
+                <p className={styles.description}>
+                  Name of crewmate <span>{item.name}</span>
+                </p>
+                <p className={styles.description}>
+                  Speed of crewmate <span>{item.speed} mph</span>
+                </p>
+                <p className={styles.description}>
+                  Color of crewmate <span>{item.color}</span>
+                </p>
+              </div>
+              <Button>Edit Crewmates</Button>
+            </div>
+          ))}
+        </div>
+      ) : null}
     </div>
-  )
+  );
 }
