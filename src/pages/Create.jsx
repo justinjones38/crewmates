f98018c4b7237449d9a5f6fdf050c08d0ead39cf
Justin <justinjones38@myyahoo.com>
2026-07-17 01:09:32 -0500

    created form component to hold form

--- src/pages/Create.jsx
+++ src/pages/Create.jsx
@@ -4,6 +4,7 @@ import amongUsImg from "../assets/among-us-lot.png";
 import { supabase } from "../services/client";
 import { useNavigate, useOutletContext } from "react-router";
 import Button from "../components/Button";
+import Form from "../components/Form";
 
 export default function Create() {
   const [inputs, setInputs] = useState({
@@ -51,113 +52,7 @@ export default function Create() {
         alt="A list of among us characters looking"
         className={styles.img}
       />
-      <form className={styles.form} onSubmit={handleSubmit}>
-        <label htmlFor="name" className={styles.textLabel}>
-          Name
-        </label>
-        <input
-          type="text"
-          value={inputs.name}
-          name="name"
-          id="name"
-          placeholder="Enter name"
-          onChange={handleChange}
-          className={styles.textInput}
-        />
-        <label htmlFor="speed" className={styles.textLabel}>
-          Speed (mph)
-        </label>
-        <input
-          type="number"
-          value={inputs.speed}
-          name="speed"
-          id="speed"
-          placeholder="Enter Speed"
-          onChange={handleChange}
-          className={styles.textInput}
-        />
-        <p className={styles.textLabel}>Color</p>
-        <div className={styles.radioContainer}>
-          <label className={styles.radioLabel}>
-            <input
-              type="radio"
-              value="red"
-              name="color"
-              id="color"
-              onChange={handleChange}
-              className={styles.radioBtn}
-              checked={inputs.color === "red"}
-            />
-            Red
-          </label>
-
-          <label className={styles.radioLabel}>
-            <input
-              type="radio"
-              value="blue"
-              name="color"
-              id="color"
-              onChange={handleChange}
-              className={styles.radioBtn}
-              checked={inputs.color === "blue"}
-            />
-            Blue
-          </label>
-
-          <label className={styles.radioLabel}>
-            <input
-              type="radio"
-              value="green"
-              name="color"
-              id="color"
-              onChange={handleChange}
-              className={styles.radioBtn}
-              checked={inputs.color === "green"}
-            />
-            Green
-          </label>
-
-          <label className={styles.radioLabel}>
-            <input
-              type="radio"
-              value="yellow"
-              name="color"
-              id="color"
-              onChange={handleChange}
-              className={styles.radioBtn}
-              checked={inputs.color === "yellow"}
-            />
-            Yellow
-          </label>
-
-          <label className={styles.radioLabel}>
-            <input
-              type="radio"
-              value="orange"
-              name="color"
-              id="color"
-              onChange={handleChange}
-              className={styles.radioBtn}
-              checked={inputs.color === "orange"}
-            />
-            Orange
-          </label>
-
-          <label className={styles.radioLabel}>
-            <input
-              type="radio"
-              value="pink"
-              name="color"
-              id="color"
-              onChange={handleChange}
-              className={styles.radioBtn}
-              checked={inputs.color === "pink"}
-            />
-            Pink
-          </label>
-        </div>
-        <Button>Submit</Button>
-      </form>
+      <Form inputs={inputs} handleChange={handleChange} handleSubmit={handleSubmit} />
     </div>
   );
 }
