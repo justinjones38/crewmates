a4ade33c6775df5be7e36d287adffcd9eed3aa6d
Justin <justinjones38@myyahoo.com>
2026-07-17 20:25:53 -0500

    changed among us char based on selection of light or dark force

--- src/components/SummaryCard.jsx
+++ src/components/SummaryCard.jsx
@@ -3,7 +3,7 @@ import styles from "./SummaryCard.module.css";
 export default function SummaryCard({ crewmates }) {
   const sum = crewmates.reduce((acc, currentVal) => acc + currentVal.speed, 0);
   const length = crewmates.length;
-  const avgVal = sum / length;
+  const avgVal = (sum / length).toFixed(2);
   const lightForcelen = crewmates.filter(
     (item) => item.force === "light",
   ).length;
